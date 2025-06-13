a3b585ac3298a761c632972fda94eecb6e769d4d
John Hoang <johnhoang1994@gmail.com>
2025-06-13 11:50:54 -0700

    Integrate Sentry for error tracking and session replay
    - Updated app.json to include Sentry configuration for error tracking.
    - Modified metro.config.js to use Sentry's Expo configuration.
    - Added Sentry initialization in _layout.tsx to wrap the application for error monitoring and session replay.
    - Updated package.json and package-lock.json to include Sentry and related dependencies.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,13 +6,33 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import * as Sentry from "@sentry/react-native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import "../global.css";
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: "https://cbb037f2e5e13123d3c6f5fab6901ed5@o4507771612626944.ingest.us.sentry.io/4508011605721088",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
