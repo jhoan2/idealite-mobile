4b274ab0d4e6fd04c4f0edb78a6d0f71f92e83a6
John Hoang <johnhoang1994@gmail.com>
2025-06-30 13:38:20 -0700

    Add index screen route to RootLayout for improved navigation
    - Introduced a new Stack.Screen for the index route, enhancing the layout structure and navigation flow within the application.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -68,6 +68,7 @@ export default Sentry.wrap(function RootLayout() {
               value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
             >
               <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
                 <Stack.Screen name="(auth)" options={{ headerShown: false }} />
                 <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                 <Stack.Screen
