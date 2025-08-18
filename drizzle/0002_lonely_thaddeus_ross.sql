b0bc0b0c7aec84ada91d13a41735c593ce9d0ca9
John Hoang <johnhoang1994@gmail.com>
2025-08-17 21:13:08 -0700

    feat: add canvas_image_cid column to pages table and update migration files

---
+++ drizzle/0002_lonely_thaddeus_ross.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `pages` ADD `canvas_image_cid` text;
--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_dusty_nehzno.sql';
 import m0001 from './0001_wise_marauders.sql';
+import m0002 from './0002_lonely_thaddeus_ross.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
